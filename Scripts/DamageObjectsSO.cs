c7882c0e8018a1141b4c94b5ebed22a74ff24efc
MasoomSayyed <masoomali5555@gmail.com>
2025-01-04 23:26:19 +0530

    Currentlatestupdate
    Have this one for latest update

---
+++ Scripts/DamageObjectsSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageObjectsSO : ScriptableObject
+{
+    public Transform prefab;
+    public Sprite sprite;
+    public string objectName;
+    public int damageAmount;
+}
