923140b127ba18f7d02d48a2941ec1d0c07d5782
Noor Zahra <114682660+NoorZahra12@users.noreply.github.com>
2025-01-09 14:23:24 +0400

    gameManager fixed
    irrelevant score texts

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -8,15 +8,8 @@ public class GameManager : MonoBehaviour
     public bool isStoryCompleted = false;
     public bool isEndSceneCompleted = false;
 
-    // Game Over
-    public GameObject gameOverScreen;
-    public TextMeshProUGUI gameOverScoreText;
-    public TextMeshProUGUI gameOverHighScoreText;
-
     //Pause Screen
     public GameObject pauseScreen;
-    public TextMeshProUGUI pauseScoreText;
-    public TextMeshProUGUI pauseHighScoreText;
 
 
     private void Awake()
@@ -45,22 +38,5 @@ public class GameManager : MonoBehaviour
         //hide pause screen
         pauseScreen.SetActive(false);
          Time.timeScale = 1;
-       // if (playerScore > savedHighScore)
-       //  {
-          //   savedHighScore = playerScore;
-         //    PlayerPrefs.SetInt("HighScore", savedHighScore); // Save new high score
-      // }
     }
-
-    /* public void GameOver()
-     {
-         gameOverScreen.SetActive(true);
-
-         // Check if we need to update the saved high score
-         if (playerScore > savedHighScore)
-         {
-             savedHighScore = playerScore;
-             PlayerPrefs.SetInt("HighScore", savedHighScore); // Save new high score
-         }
-     }*/
 }
